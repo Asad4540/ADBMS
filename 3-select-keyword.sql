1b12cbac1fa3a6168baa34ec4597fab3ab955900
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-10-23 02:47:41 +0530

    select-query

---
+++ 3-select-keyword.sql
@@ -0,0 +1,4 @@
+SELECT * FROM teachers; --To display all data
+SELECT firstname from teachers; --To display only firstname from teachers
+SELECT lastname,salary,hire_date from teachers; --To display lastname,salary & hire_date 
+
