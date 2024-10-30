55497c6b88af92a640447e013ca5c7523b645f1f
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-10-31 02:52:40 +0530

    where query

---
+++ 5-where.sql
@@ -0,0 +1,10 @@
+select school,lastname,hire-date from teachers
+-- where school = "don-bosco"
+WHERE school = 'don bosco' and lastname = 'chaudhary'
+and hire_date >= '2024-05-26';
+
+-- =
+-- <>
+-- !=
+-- <=
+-- >=
